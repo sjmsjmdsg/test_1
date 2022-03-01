43dda36f4d9582fedbe1dedf2f0ee7d76aeddb4c
anonymous <email@example.com>
2022-03-02 10:09:28 +1100

    first commit

---
+++ code_test.py
@@ -0,0 +1,6 @@
+# Imported into code using
+tax2 = 11
+from finance import *
+
+print(tax1)
+print(tax2)
---
+++ finance.py
@@ -0,0 +1,5 @@
+__all__ = ['tax1', 'tax2']  #defines the names to import when '*' is used
+
+tax1 = 5
+tax2 = 10
+def cost(): return 'cost'
