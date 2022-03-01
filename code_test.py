9cd30fbc16e097fd9df020cd77ec9e17016ace50
anonymous <email@example.com>
2022-03-02 10:29:47 +1100

    3 commit

--- code_test.py
+++ code_test.py
@@ -4,3 +4,16 @@ from finance import *
 tax2 = 11
 print(tax1)
 print(tax2)
+
+
+import re
+def filterScriptTags(content):
+    oldContent = ""
+    while oldContent != content:
+        oldContent = content
+        content = re.sub(r'<script.*?>.*?</script>', '', content, flags= re.DOTALL | re.IGNORECASE)
+    return content
+
+
+if __name__ == '__main__':
+    filterScriptTags('asdasd')
